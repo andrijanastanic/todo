14dd6a0d80c57db4e68643f03e0fd3e3bf569129
unknown <andrijana.stanic94@gmail.com>
2019-04-16 22:18:09 +0200

    proba2

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
-  :repl-options {:init-ns todo-list.core})
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [ring                "1.7.1"]]
+  :repl-options {:init-ns todo-list.core}
+  :main todo-list.core)
